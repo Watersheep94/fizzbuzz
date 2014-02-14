4e821cc20e480757a38e784598f362096c8c5cb7
Michael Liu <watersheep94@gmail.com>
2014-02-13 18:30:36 -0800

    Created fizzbuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,17 @@
+number = 0
+
+while number != 100:
+
+	number += 1
+
+	multThree = number % 3 == 0
+	multFive = number % 5 == 0
+
+	if multThree and multFive:
+		print "Fizzbuzz"
+	elif multThree:
+		print "Fizz"
+	elif multFive:
+		print "Buzz"
+	else:
+		print number
